3f6b34689b1eec9af1a084a70aa7637e5f855b71
cyf <1012383516@qq.com>
2019-02-24 21:58:59 +0800

    22

---
+++ gdx_parent/gdx_basic_parent/gdx_basic_util/src/test/java/TreeNode.java
@@ -0,0 +1,16 @@
+/**
+ * @author cyf
+ * @date 2019/2/23 21:29
+ * @description
+ */
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
